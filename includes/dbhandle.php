d0be08819311463bf3a6805ba5a5f5ff3853021e
Mumbere Godwin <godwinmumbere3@gmail.com>
2024-12-10 17:31:12 +0300

    Update dbhandle.php

--- includes/dbhandle.php
+++ includes/dbhandle.php
@@ -1,9 +1,9 @@
 <?php      
     $host = "localhost";  
-    $user = "root";  
-    $passw = "";  
-    $dbname = "dtehm";  
-
+    $user = "dtehmhea_appadmin";  
+    $passw = "w0ejk3NM)n0j";  
+    $dbname = "dtehmhea_app";  
+      
     $con = mysqli_connect($host, $user, $passw, $dbname);  
     if( mysqli_connect_errno()) {  
         die("Failed to connect with MySQL: ". mysqli_connect_error());  
